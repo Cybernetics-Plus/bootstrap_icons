78eb97f6061510c8c7fc2a81437e8d1043254795
BOMEN <bomen21x@gmail.com>
2022-08-01 23:36:15 +0700

    Start Odoo Module Bootstrap Icon 1.9.0
    Start Odoo Module Bootstrap Icon 1.9.0

---
+++ ctp_bootstrap_icons/__manifest__.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Cybernetics Plus Co., Ltd.
+#    Cybernetics Plus Install Bootstrap Icons 1.9.0 in Odoo
+#
+###################################################################################
+
+{
+    'name': 'Bootstrap Icons',
+    'version': '15.1.9.0.1',
+    'summary': """ 
+            Cybernetics Plus Install Bootstrap Icons 1.9.0 in Odoo
+            .""",
+    'description': """ 
+            Cybernetics Plus Install Bootstrap Icons 1.9.0 in Odoo
+            .""",
+    'author': 'Cybernetics Plus Co., Ltd.',
+    'website': 'https://www.cybernetics.plus',
+    'live_test_url': 'https://www.cybernetics.plus',
+    'images': ['static/description/banner.png'],
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'contributors': [
+        'Developer <dev@cybernetics.plus>',
+    ],
+    'assets': {
+        'web.assets_qweb': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_common_minimal': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_common': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_common_lazy': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_backend': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        "web.assets_backend_legacy_lazy": [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_frontend': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_frontend_minimal': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_frontend_lazy': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.assets_backend_prod_only': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.report_assets_common': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+        'web.report_assets_pdf': [
+            'ctp_bootstrap_icons/static/src/css/bootstrap-icons.css',
+        ],
+    },
+}
